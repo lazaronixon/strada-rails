ee2b9cbe2cdadb7ba72df647875cb07f28a392df
Nixon <lazaronixon@hotmail.com>
2023-09-29 03:46:10 -0300

    Fix HIDE_ON_MOBILE_STYLE

--- lib/install/strada_with_node.rb
+++ lib/install/strada_with_node.rb
@@ -1,11 +1,19 @@
+APPLICATION_LAYOUT_PATH = Rails.root.join("app/views/layouts/application.html.erb")
+
+HIDE_ON_MOBILE_STYLE = <<-HTML.chomp
+    <% if turbo_native_app? %>
+      <style>.hide-on-mobile { display: none; }</style>
+    <% end %>
+HTML
+
 say "Copy bridge controllers"
 directory "#{__dir__}/app/javascript/controllers/bridge", "app/javascript/controllers/bridge"
 
 say "Copy strada stylesheet"
 copy_file "#{__dir__}/app/assets/stylesheets/strada.css", "app/assets/stylesheets/strada.css"
 
-say "Copy application layout"
-template "#{__dir__}/app/views/layouts/application.html.erb", "app/views/layouts/application.html.erb", force: true
+say "Add hide-on-mobile style in application layout"
+insert_into_file APPLICATION_LAYOUT_PATH.to_s, "\n\n#{HIDE_ON_MOBILE_STYLE}", before: /\s*<\/head>/
 
 say "Install Strada"
 run "yarn add @hotwired/strada"
