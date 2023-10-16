1e3c9a2db9a4681ec4086946207b9bd99a9790c7
Nixon <lazaronixon@hotmail.com>
2023-10-16 01:13:12 -0300

    add strada.css to application.*.scss

--- lib/install/strada_with_importmap.rb
+++ lib/install/strada_with_importmap.rb
@@ -6,11 +6,19 @@ HIDE_ON_MOBILE_STYLE = <<-HTML.chomp
     <% end %>
 HTML
 
+destination = Pathname(destination_root)
+
 say "Copy bridge controllers"
 directory "#{__dir__}/app/javascript/controllers/bridge", "app/javascript/controllers/bridge"
 
 say "Copy strada stylesheet"
 copy_file "#{__dir__}/app/assets/stylesheets/strada.css", "app/assets/stylesheets/strada.css"
+
+unless destination.join("app/assets/application.css").exist?
+  if (stylesheets = Dir.glob "#{destination_root}/app/assets/stylesheets/application.*.{scss,css}").length > 0
+    insert_into_file stylesheets.first.to_s, %(@import 'strada.css';)
+  end
+end
 
 say "Add hide-on-mobile style in application layout"
 insert_into_file APPLICATION_LAYOUT_PATH.to_s, "\n\n#{HIDE_ON_MOBILE_STYLE}", before: /\s*<\/head>/
